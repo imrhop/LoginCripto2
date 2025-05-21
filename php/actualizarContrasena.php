bdccd0a8c06b894510f9c627674da299ceea4809
imrhop <ivanmaldortiz@gmail.com>
2025-05-21 00:14:57 -0600

    new2

--- php/actualizarContrasena.php
+++ php/actualizarContrasena.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-// require_once 'conexion.php';
-require_once '../db.php';
+require_once 'conexion.php';
 
 function limpiar($dato) {
     return htmlspecialchars(trim($dato));
