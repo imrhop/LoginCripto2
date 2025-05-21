492e8ec7e4023f7fcb0138e640f35bc88ca13868
imrhop <ivanmaldortiz@gmail.com>
2025-05-21 02:02:05 -0600

    Arreglado1

--- php/conexion.php
+++ php/conexion.php
@@ -4,6 +4,11 @@ $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");
 $db   = getenv("DB_NAME");
 
+// Mostrar el valor de cada variable
+echo "DB_HOST: " . $host . "<br>";
+echo "DB_USER: " . $user . "<br>";
+echo "DB_PASS: " . $pass . "<br>";
+echo "DB_NAME: " . $db . "<br>";
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
