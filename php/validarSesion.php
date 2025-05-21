1e8e65ddb3dd65128add22d75961cc58b2046b7e
Yael Galvan <168500000+GOGA912@users.noreply.github.com>
2025-05-20 21:08:21 -0600

    Update validarSesion.php

--- php/validarSesion.php
+++ php/validarSesion.php
@@ -3,7 +3,13 @@ session_start();
 
 // Si ya hay una sesión iniciada, redirige al inicio del sistema
 if (isset($_SESSION["usuario_id"])) {
-    header("Location: inicio.php");
+    if ($_SESSION["estado"] === 1) {
+        header("Location: inicio.php");
+    } else if ($_SESSION["estado"] === 2) {
+        header("Location: ../html/cambiarContrasena.html");
+    } else {
+        header("Location: ../html/validarCuenta.html");
+    }
     exit;
 }
 ?>
