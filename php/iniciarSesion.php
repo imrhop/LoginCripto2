5d24a7e15c700587a74ecd3398fb4aa8f149979b
ImaTrunks5 <152110124+ImaTrunks5@users.noreply.github.com>
2025-05-21 18:46:35 -0600

    incio sesion

--- php/iniciarSesion.php
+++ php/iniciarSesion.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 	<div class="navbar">
-		<a href="../index.html" class="logo"><img src="../img/Divan.png"></a>
+		<a href="../index.php" class="logo"><img src="../img/Divan.png"></a>
 		<p style=' color: #ffffff;font-style: italic; text-align: center; margin-top: 20px; margin-left: 5px;'>
                 Protege tus datos, cifra tu mundo
         	</p>
